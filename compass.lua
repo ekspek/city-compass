9db0161c88fc832060a0bf502250c71c3ed9af79
Daniel de Schiffart <danieldeschiffart@gmail.com>
2019-07-14 17:54:31 +0200

    Add test values

--- compass.lua
+++ compass.lua
@@ -1,9 +1,9 @@
-function direction(p1, p2)
+function direction(p1in, p2in)
 	-- Import two coordinates as tables
-	local p1.lat = p1[1]
-	local p1.lon = p1[2]
-	local p2.lat = p2[1]
-	local p2.lon = p2[2]
+	p1.lat = p1[1]
+	p1.lon = p1[2]
+	p2.lat = p2[1]
+	p2.lon = p2[2]
 
 	-- Radial distance between the two points
 	local raddist = math.acos(math.cos(p2.lat) * math.cos(p1.lon - p2.lon) * math.cos(p1.lat) + math.sin(p2.lat) * math.sin(p1.lat))
@@ -15,6 +15,8 @@ function direction(p1, p2)
 	local heading2 = math.atan((-math.cos(p1.lat) * math.sin(p1.lon - p2.lon)) / (math.sin(p2.lat) * math.cos(p1.lon - p2.lon) * math.cos(p1.lat) - math.cos(p2.lat) * math.sin(p1.lat)))
 
 	return heading1
+	--return heading2
+	--return raddist * 6378000
 end
 
 function dms2deg(d, m, s)
@@ -26,3 +28,12 @@ function deg2rad(deg)
 	local rad = deg * math.pi / 180
 	return rad
 end
+
+function rad2deg(rad)
+	local deg = rad * 180 / math.pi
+	return rad
+end
+
+p1 = { deg2rad(dms2deg(40, 45, 0)), deg2rad(dms2deg(-73, -58, 0)) }
+p2 = { deg2rad(dms2deg(51, 32, 0)), deg2rad(dms2deg(0, -10, 0)) }
+print(direction(p1, p2))
