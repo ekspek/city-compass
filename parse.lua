18efe2abc12891c94482f10c76b1744abc58c9ee
Daniel de Schiffart <danieldeschiffart@gmail.com>
2019-07-20 12:39:12 +0200

    Add csv file parsing using OpenFlight format

--- parse.lua
+++ parse.lua
@@ -1,3 +1,4 @@
+local csv = require 'lua-csv.lua.csv'
 local cmp = require 'compass'
 
 local parse = {}
@@ -18,6 +19,30 @@ parse.deg2rad = function(coordlist)
 	for i = 1,#coordlist do
 		coordlist[i].latitude = cmp.deg2rad(coordlist[i].latitude)
 		coordlist[i].longitude = cmp.deg2rad(coordlist[i].longitude)
+	end
+
+	return coordlist
+end
+
+parse.file = function(filename)
+	local coordlist = {}
+
+	local file = csv.open(filename)
+
+	for fields in file:lines() do
+		local coord = {}
+		for j, v in ipairs(fields) do
+			if j == 3 then coord.name = v end
+			if j == 7 then coord.latitude = v end
+			if j == 8 then coord.longitude = v end
+		end
+		table.insert(coordlist, coord)
+	end
+
+	for _, u in ipairs(coordlist) do
+		for i, v in pairs(u) do
+			print(i, v)
+		end
 	end
 
 	return coordlist
